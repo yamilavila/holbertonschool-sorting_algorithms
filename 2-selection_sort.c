148e29518e0e8cd7f2496f6d9fe6a74227aa5c72
yamilavila <git@github.com>
2022-06-15 00:10:28 +0000

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
